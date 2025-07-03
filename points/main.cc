1d56775738dded6e8c7158e8fba8f2e4305c35d1
StudioAlbert <account@studio-albert.com>
2025-07-03 12:30:42 +0200

    fix mistyping

--- points/main.cc
+++ points/main.cc
@@ -17,7 +17,6 @@ struct Point {
     float y_;
 
     Type type_;
-
 };
 
 class PointCloud {
@@ -25,10 +24,13 @@ class PointCloud {
 
 public:
     void Fill();
+
     auto NearPoints(Point p, float radius);
+
     auto AllPoints(){
         return points_;
     }
+
     auto GetTruc();
 };
 
@@ -40,33 +42,37 @@ inline float RandomRange(){
 }
 
 inline Type RandomType(){
-
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(0, static_cast<int>(Type::kLength) - 1);
     return static_cast<Type>(dis(gen));
-
 }
 
 inline float Distance(Point p1, Point p2){
     float a = p2.x_ - p1.x_;
     float b = p2.y_ - p1.y_;
-    return std::sqrt(a*a + b*b);
+    return std::sqrt(a * a + b * b);
 }
 
 inline void PointCloud::Fill(){
-    std::ranges::generate(points_, [](){return Point{RandomRange(), RandomRange(), RandomType()};});
+    std::ranges::generate(points_, []() { return Point{RandomRange(), RandomRange(), RandomType()}; });
 }
 
 inline auto PointCloud::NearPoints(Point p, float radius){
-    return points_
-    | std::views::filter([p, radius](auto q){return Distance(p, q) <= radius;})
-    | std::views::filter([](auto q) {return q.type_ == Type::kTriangle;});
+    // return points_
+    // | std::views::filter([p, radius](auto q){return Distance(p, q) <= radius;})
+    // | std::views::filter([](auto q) {return q.type_ == Type::kTriangle;});
+
+
+    return std::views::filter(points_, [](auto p) { return p.type_ == Type::kTriangle; });
 }
 
 inline auto PointCloud::GetTruc(){
     return points_
-    | std::views::transform([](Point p){return p.type_;}); main(){
+           | std::views::transform([](Point p) { return p.type_; });
+}
+
+int main(){
     std::cout << "Hello World!" << std::endl;
 
     PointCloud pc;
